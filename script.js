724ca06ad775b302e1cdb84e83f9a66be0bb174a
Ryan Meeks <ryan_meeks613@gmail.com>
2018-04-02 15:30:29 -0700

    [more css changes, js clean up, slightly]

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ window.addEventListener('error', function(e) {
     console.log(e.target);
 }, true);
 
-function getDataFromApi(artist, title, callback) {
-   
+//API Call to GET lyrics
+function getLyricDataFromApi(artist, title, callback) {
     const newURL = `${LYRICS_URL}/${artist}/${title}`;
+
     $.getJSON(newURL, callback);
+    
     const youTubeString = `${artist} - ${title}`;
     getYoutubeDataFromApi(youTubeString, displayYoutubeApiData);
 }
 
+//DISPLAY LYRICS
+function displaySearchData(data2) {
+    const results = data2.lyrics;
+    getSwearApi(results, displaySwearApiData);
+         $(".js-search-results").append(results);
+   }
+
+//API CALL to get YouTube Video
 function getYoutubeDataFromApi(song, callback3) {
     const query = {
         key: "AIzaSyCOFx-c_YF08zb4utaaxy4yaTrmiR80xaY",
@@ -22,30 +32,24 @@ function getYoutubeDataFromApi(song, callback3) {
         part: 'snippet'
        };
         $.getJSON(YOUTUBE_URL, query, callback3);
-
-    
-   
 }
+
+//DISPLAY YouTube API Data
 function displayYoutubeApiData(data3) {
     if ( data3.items.length > 0) {
-        
-    
-    console.log(data3);
     const displayTitle = data3.items[0].snippet.title;
     const videoUrl = data3.items[0].id.videoId;
-    console.log(displayTitle);
-    
-
     $(".title").html(`<h2>${displayTitle}</h2>`);
     $(".js-search-results").html(`<iframe class="video" src="https://www.youtube.com/embed/${videoUrl}" frameborder="0"" allowfullscreen></iframe>`);
     $(".main-title").hide();
     $(".submit").html("Search Again");
     }
     else {
-        $(".title").html('<h2>There are no results. Please try again!</h2>');
+        $(".title").html('<h2>There are no results. Please check your spelling and try again!</h2>');
     }
 }
 
+//API CALL to check lyrics for swears
 function getSwearApi(lyrics, callback2) {
     const query = {
         userId: "ryanmeeks613",
@@ -55,43 +59,42 @@ function getSwearApi(lyrics, callback2) {
         "censor-character": "%"
     }
     $.getJSON(SWEARS_URL, query, callback2);
-
 }
-    
+
+//DISPLAY SWEAR API Data
 function displaySwearApiData(data) {
     
     const count = data['bad-words-total'];
-    const swearWords = data['bad-words-list'].join(", ");
-    
+    const swearWords = data['bad-words-list'].join(" | ");
+        
     if (count === 0) {
-        $(".swear-results").html("<h2><strong>No Swears! Play Away!</strong></h2>");
-        $("h2").removeClass().addClass("green");
+        $(".swear-results").html(`<p style="background-color: green">This song contains 0 swears. Play away, but check the lyrics for inappropriate innuendos!</p>`);
+        
+    }
+
+    else if (count === 1) {
+        $(".swear-results").html(`<p style="background-color:red"><strong class="blink_me">WARNING!!</strong> This song contains only one swear word. It is <strong>${swearWords}</strong>.</p>`);
+        
     }
 
     else {
-        $(".swear-results").html(`<p><strong>WARNING!!</strong> This song contains ${count} swear words. They are ${swearWords}`);
-        $("h2").removeClass().addClass("red");
+        $(".swear-results").html(`<p style="background-color:red"><strong class="blink_me">WARNING!!</strong> This song contains ${count} swear words. They are <strong>${swearWords}</strong>.</p>`);
+        
     }
-}
+} 
 
-function displaySearchData(data2) {
- const results = data2.lyrics;
- 
- getSwearApi(results, displaySwearApiData);
-      $(".js-search-results").append(results);
 
- console.log(data2);
-}
 
 //watches for the form to be submitted
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     //prevent it from resetting
     event.preventDefault();
-    //save as constants to clear out form after submission.
+    
     $(".title").empty();
     $(".js-search-results").empty();
     $(".swear-results").empty();
+    //save as constants to clear out form after submission.
     const artistInput = $(this).find('.input-artist');
     const titleInput = $(this).find('.input-title');
     const artist = artistInput.val().trim();
@@ -99,7 +102,7 @@ function watchSubmit() {
     artistInput.val('');
     titleInput.val('');
     
-    getDataFromApi(artist, title, displaySearchData);
+    getLyricDataFromApi(artist, title, displaySearchData);
   });
 }
 
