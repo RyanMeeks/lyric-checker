e9b759027038b63933dae0e9e672b50b529ede4f
Ryan Meeks <ryan_meeks613@gmail.com>
2018-04-02 16:00:29 -0700

    [more axe/ally changes]

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ const LYRICS_URL = "https://private-anon-b1f27724d6-lyricsovh.apiary-proxy.com/v
 const SWEARS_URL = "https://cryptic-badlands-95093.herokuapp.com/";
 const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search";
 
-window.addEventListener('error', function(e) {
-    console.log(e);
-    console.log(e.target);
-}, true);
-
 //API Call to GET lyrics
 function getLyricDataFromApi(artist, title, callback) {
     const newURL = `${LYRICS_URL}/${artist}/${title}`;
@@ -21,7 +16,7 @@ function getLyricDataFromApi(artist, title, callback) {
 function displaySearchData(data2) {
     const results = data2.lyrics;
     getSwearApi(results, displaySwearApiData);
-         $(".js-search-results").append(results);
+         $(".lyrics").append(results);
    }
 
 //API CALL to get YouTube Video
@@ -31,18 +26,18 @@ function getYoutubeDataFromApi(song, callback3) {
         q: song,
         part: 'snippet'
        };
-        $.getJSON(YOUTUBE_URL, query, callback3);
+    $.getJSON(YOUTUBE_URL, query, callback3);
 }
 
 //DISPLAY YouTube API Data
 function displayYoutubeApiData(data3) {
     if ( data3.items.length > 0) {
-    const displayTitle = data3.items[0].snippet.title;
-    const videoUrl = data3.items[0].id.videoId;
-    $(".title").html(`<h2>${displayTitle}</h2>`);
-    $(".js-search-results").html(`<iframe class="video" src="https://www.youtube.com/embed/${videoUrl}" frameborder="0"" allowfullscreen></iframe>`);
-    $(".main-title").hide();
-    $(".submit").html("Search Again");
+        const displayTitle = data3.items[0].snippet.title;
+        const videoUrl = data3.items[0].id.videoId;
+        $(".title").html(`<h2>${displayTitle}</h2>`);
+        $(".js-search-results").html(`<iframe class="video" src="https://www.youtube.com/embed/${videoUrl}" title="${displayTitle}" frameborder="0" allowfullscreen></iframe>`);
+        $(".main-title").hide();
+        $(".submit").html("Search Again");
     }
     else {
         $(".title").html('<h2>There are no results. Please check your spelling and try again!</h2>');
@@ -63,27 +58,21 @@ function getSwearApi(lyrics, callback2) {
 
 //DISPLAY SWEAR API Data
 function displaySwearApiData(data) {
-    
     const count = data['bad-words-total'];
     const swearWords = data['bad-words-list'].join(" | ");
         
     if (count === 0) {
         $(".swear-results").html(`<p style="background-color: green">This song contains 0 swears. Play away, but check the lyrics for inappropriate innuendos!</p>`);
-        
     }
 
     else if (count === 1) {
-        $(".swear-results").html(`<p style="background-color:red"><strong class="blink_me">WARNING!!</strong> This song contains only one swear word. It is <strong>${swearWords}</strong>.</p>`);
-        
+        $(".swear-results").html(`<p style="background-color:red"><strong class="blink_me">WARNING!!</strong> This song contains only one swear word. It is <strong>${swearWords}</strong>.</p>`);  
     }
 
     else {
         $(".swear-results").html(`<p style="background-color:red"><strong class="blink_me">WARNING!!</strong> This song contains ${count} swear words. They are <strong>${swearWords}</strong>.</p>`);
-        
     }
 } 
-
-
 
 //watches for the form to be submitted
 function watchSubmit() {
@@ -101,14 +90,9 @@ function watchSubmit() {
     const title = titleInput.val().trim();
     artistInput.val('');
     titleInput.val('');
-    
     getLyricDataFromApi(artist, title, displaySearchData);
   });
 }
 
 
 $(watchSubmit);
-
-//make function to call api,
-// look at the output..
-// figure out how to put it on the screen. 
